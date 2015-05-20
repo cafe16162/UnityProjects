33e3c760fc437440bd8e046e3bfdca02120140db
cafe16162 <cafe16162@gmail.com>
2015-05-20 17:03:23 +0900

    EffectsComponentTestプロジェクトを追加

---
+++ EffectsComponentTest/Assets/LineRenderer/DrawLine.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DrawLine : MonoBehaviour
+{
+	[SerializeField]
+	LineRenderer lineRenderer;
+
+	[SerializeField]
+	bool isActive = true;
+	public bool IsActive{ set{ isActive = value; } get{ return isActive; } }
+
+	int vc = 0;		// Vertex Counts
+	enum DrawState
+	{
+		idle,
+		drawing,
+	};
+	DrawState state;
+
+	void Start()
+	{
+		state = DrawState.idle;
+		vc = 0;
+		lineRenderer.SetVertexCount(vc);
+		lineRenderer.useWorldSpace = true;
+	}
+	/*
+	void Update()
+	{
+		if (!isActive) return;
+
+		if (Input.GetMouseButtonDown(0))
+		{
+			state = DrawState.drawing;
+			lineRenderer.SetVertexCount(++vc);
+			lineRenderer.SetPosition(vc-1, Camera.main.ScreenToWorldPoint(Input.mousePosition));
+		}
+		else if (Input.GetMouseButton(0) && state == DrawState.drawing)
+		{
+			// itteinonagasaijounara
+		}
+	}
+	*/
+}
---
+++ EffectsComponentTest/Assets/TrailRenderer/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+	[SerializeField]
+	float speed = 5f;
+
+	Vector2 dv;
+
+	void Update()
+	{
+		dv.x = Input.GetAxis("Horizontal");
+		dv.y = Input.GetAxis("Vertical");
+
+		transform.Translate((Vector3)dv * Time.deltaTime * speed);
+	}
+}
